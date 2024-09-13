8c8fa9f902b74e88265360c31c8d23404ab9f783
Joseph Simpson <joe@radicalappdev.com>
2024-09-13 18:33:29 -0400

    Initial Commit

---
+++ ABrightIdea/ABrightIdeaApp.swift
@@ -0,0 +1,33 @@
+//
+//  ABrightIdeaApp.swift
+//  ABrightIdea
+//
+//  Created by Joseph Simpson on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct ABrightIdeaApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
