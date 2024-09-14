51c6ac2112dfa34ad274499d9046eb8fd3434a37
Joseph Simpson <joe@radicalappdev.com>
2024-09-14 06:45:14 -0400

    Cycle through light presets

--- ABrightIdea/ImmersiveView.swift
+++ ABrightIdea/ImmersiveView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 import RealityKit
 import RealityKitContent
 
+//enum LightType: Float {
+//    case dim =  520
+//    case regular = 1600
+//    case bright = 3200
+//}
+
 enum LightType: Float {
-    case dim =  13481.88
+    case dim =  6740.94
     case regular = 26963.76
     case bright = 53927.52
 }
@@ -30,33 +36,44 @@ struct ImmersiveView: View {
                     dome.scale = .init(x: -1, y: 1, z: 1)
                 }
 
-                if let lightSource = root.findEntity(named: "LightBulb") {
-                    for _ in 0..<7 {
-                        let lightSourceCopy = lightSource.clone(recursive: true)
-                        let randomX = Float.random(in: -2...2)
-                        let randomY = Float.random(in: 0...2)
-                        let randomZ = Float.random(in: -2...2)
-
-                        lightSourceCopy.position = SIMD3(x: randomX, y: randomY, z: randomZ)
-
-                        if let lightSource = lightSourceCopy.findEntity(named: "LightSource") {
-                            if var pointLight = lightSource.components[PointLightComponent.self] {
-                                pointLight.intensity = Float.random(in: 1000...53927.52)
-                                print("LIGHT Intensity: \(pointLight.intensity)")
-                                lightSource.components.set(pointLight)
-                            }
-
-                        }
-
-                        content.add(lightSourceCopy)
-                    }
-                }
+//                if let lightSource = root.findEntity(named: "LightBulb") {
+//                    for _ in 0..<7 {
+//                        let lightSourceCopy = lightSource.clone(recursive: true)
+//                        let randomX = Float.random(in: -2...2)
+//                        let randomY = Float.random(in: 0...2)
+//                        let randomZ = Float.random(in: -2...2)
+//
+//                        lightSourceCopy.position = SIMD3(x: randomX, y: randomY, z: randomZ)
+//
+//                        if let lightSource = lightSourceCopy.findEntity(named: "LightSource") {
+//                            if var pointLight = lightSource.components[PointLightComponent.self] {
+//                                pointLight.intensity = Float.random(in: 1000...53927.52)
+//                                print("LIGHT Intensity: \(pointLight.intensity)")
+//                                lightSource.components.set(pointLight)
+//                            }
+//
+//                        }
+//
+//                        content.add(lightSourceCopy)
+//                    }
+//                }
             }
         } update: { content in
 
-//            if let rootEntity = content.entities.first {
-//                // TODO
-//            }
+            if let root = content.entities.first {
+                if let lightSource = root.findEntity(named: "LightBulb") {
+
+                    if let lightSource = lightSource.findEntity(named: "LightSource") {
+                        if var pointLight = lightSource.components[PointLightComponent.self] {
+                            pointLight.intensity = lightType.rawValue
+                            print("LIGHT Intensity: \(pointLight.intensity)")
+                            lightSource.components.set(pointLight)
+                        }
+
+                    }
+
+                }
+            }
 
         }
         .gesture(tapGesture)
